06f8b844cacd552d7ceb9be0e948901e2b76999c
vanessavela <vanessa@sirius.utp.edu.co>
2017-03-16 14:55:17 -0500

    homework progress 30/32

--- exercises.js
+++ exercises.js
@@ -91,14 +91,11 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
-  for (var i = 2; i < num; i++){
-    if (num % i !== 0) {
-      return true;
-    }  return false;
-
-  }
-
-
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  } return false;
   //return true if num is prime.
   //otherwise return false
   //hint: a prime number is only evenly divisible by itself and 1
@@ -161,25 +158,38 @@ function contains(arr, item) {
   for (var i = 0; i < arr.length; i++) {
     if(arr[i] === item) {
       return true;
-    }else {
-      return false;
     }
-  }
+  } return false;
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
+  var sum=0;
+  for (var i=0; i<numbers.length;i++){
+    sum=sum+numbers[i];
+  }return sum;
   //numbers is an array of integers.
   //add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
+  var sum=0;
+  var aver=0;
+  for (var i=0; i<testScores.length;i++){
+    aver=(sum=sum+testScores[i])/testScores.length;
+  }return aver;
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
 }
 
 function largestNumber(numbers) {
+  var largest = 0;
+  for (var i = 0; i<numbers.length; i++) {
+    if (numbers[i]>largest){
+    largest = numbers[i];
+  }return largest;
+}
   //numbers is an array of integers
   //return the largest integer
 }
